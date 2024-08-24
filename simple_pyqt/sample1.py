421a66fdf24fbac42dfb1c4587347eedadfcb495
wally <goldenale5500@gmail.com>
2024-08-24 16:19:46 +0900

    pyqt5 테스트

---
+++ simple_pyqt/sample1.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle('PyQt6 App')
+window.setGeometry(100, 100, 280, 80)
+
+helloMsg = QLabel('<h1>Hello, PyQt5!</h1>', parent=window)
+helloMsg.move(60, 15)
+
+window.show()
+
+sys.exit(app.exec())
